61a21bb6c3a6d0b8fce9c32b7abf1add36199792
tsyganno <tsyganlee@gmail.com>
2022-07-08 20:45:49 +0300

    second

---
+++ decorator_with_parameters.py
@@ -0,0 +1,28 @@
+def timer(call_count, start_sleep_time=1, factor=4, border_sleep_time=5):
+    def function(func):
+        def inner(*args):
+            t = start_sleep_time
+            print(f'Кол-во запусков = {call_count}')
+            print('Начало работы')
+            count = 1
+            for _ in range(call_count):
+                if t < border_sleep_time:
+                    t = start_sleep_time * 2 ** factor
+                    print(f'Запуск номер {count}. Ожидание: {t} секунд. Результат декорируемой функций = {func(*args)}')
+                    count += 1
+                else:
+                    t = border_sleep_time
+                    print(f'Запуск номер {count}. Ожидание: {t} секунд. Результат декорируемой функций = {func(*args)}')
+                    count += 1
+            print('Конец работы')
+        return inner
+    return function
+
+
+@timer(3)
+def multiplier(number: int):
+    return number * 2
+
+
+if __name__ == '__main__':
+    multiplier(2)
